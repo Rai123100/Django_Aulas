7f011ec2f45fb8cda2fb2c86d607ace86a933a68
Rai <raicarvalho343@gmail.com>
2025-09-01 16:52:34 -0300

    Primeira parte finalizada - criadas as tabelas/models

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class especialidade(models.Model):
+    id_especialidade = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+class medico(models.Model):
+    id_medico = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField()
+    crm = models.CharField(max_length=50, unique=True)
+    id_especialidade = models.ForeignKey(especialidade, on_delete=models.CASCADE, related_name="medico")
+
+    def __str__(self):
+        return f"{self.nome} - {self.id_especialidade.nome}"
